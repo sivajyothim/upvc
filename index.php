538d0e66d890286d401b9c9f7ad7277fe1efe514
anji <anji.naga1@gmail.com>
2019-01-29 23:15:10 +0530

    contact number added in comminsoon page

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	<link rel="stylesheet" href="assets/css/comming-soon.css">
 </head>
 <body>
-<h3 class="content text-capitalize">our new awesome website is comming soon ...</h3>	
+<h3 class="content text-capitalize">our new awesome website is comming soon ...</h3>
+<p class="text-capitalize">please contact for any information kishore phone : +91-9640086373</p>
 </body>
 </html>
