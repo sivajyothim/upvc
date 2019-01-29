8e8e1438f5d031e6609a79756899067dd36393f2
jyothi <jyothi..com>
2019-01-29 10:12:36 +0530

    comming soon page

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/comming-soon.css">
 </head>
 <body>
-website under counstuction
+<h3 class="content text-capitalize">our new awesome website is comming soon ...</h3>	
 </body>
 </html>
